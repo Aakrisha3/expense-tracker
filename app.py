665527bfc084e2366fb2c56274ad9da1b9d7f7fb
Aakrisha3 <aakrisha83@gmail.com>
2026-05-22 19:04:27 +0545

    Added delete expense feature

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template ,request
 
 app = Flask(__name__)
 
-@app.route('/')
+expenses =[]
+
+@app.route('/', methods=['GET','POST'])
 def home():
-    return render_template('index.html')
+    if request.method == 'POST':
+        title = request.form['title']
+        amount = request.form['amount']
+
+
+        expense = {
+            'title': title,
+            'amount': amount
+        }
+        expenses.append(expense)
+    return render_template('index.html',expenses=expenses)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
+ 
